e2d7fcdb9498d7fae5535aa673b393e38ebbc4a5
Edwin Rosario <edwin.rosario@andulan.com>
2023-01-31 00:50:02 -0500

    Update conf.py
    update author information.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'TestHello'
+copyright = '2023, Splash Paint Production'
+author = 'Rosa Rio'
 
 release = '0.1'
 version = '0.1.0'
